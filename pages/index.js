8da30e557d1624ff85613f4aa4cec56cc960ce7d
Matt <m@ttjohnston.com>
2021-02-28 23:30:52 -0400

    add favicon data

--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,32 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Palettable</title>
-        <link rel="icon" href="/favicon.ico" />
-        
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="theme-color" content="#000000" />
       </Head>
       <main className={styles.main}>
-        <pre style={{textAlign:'center',verticalAlign:'center'}}>palettable</pre>
+        <pre style={{ textAlign: "center", verticalAlign: "center" }}>
+          palettable
+        </pre>
         {palettable.colors.map((paletted, idx) => {
           const description = Object.keys(paletted)[0];
           const color = paletted[description];
@@ -26,17 +47,27 @@ export default function Home() {
               }}
               key={`palettable_${idx}`}
             >
-              <div style={{ position: "relative", top: "50%"}}>
+              <div style={{ position: "relative", top: "50%" }}>
                 <span>
                   {description} - {color}{" "}
                 </span>
-                <div style={{marginTop:24}}><CopyButton colorToCopy={color} /></div>
+                <div style={{ marginTop: 24 }}>
+                  <CopyButton colorToCopy={color} />
+                </div>
               </div>
             </section>
           );
         })}
       </main>
-      {palettable.githubUsername ? <pre><a href={`https://github.com/${palettable.githubUsername}/palettable`}>github.com/{palettable.githubUsername}/palettable</a></pre> : null}
+      {palettable.githubUsername ? (
+        <pre>
+          <a
+            href={`https://github.com/${palettable.githubUsername}/palettable`}
+          >
+            github.com/{palettable.githubUsername}/palettable
+          </a>
+        </pre>
+      ) : null}
     </div>
   );
 }
